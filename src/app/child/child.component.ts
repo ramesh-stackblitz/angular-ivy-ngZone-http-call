6c29443ac1101772a08c5ba586a0fe20ea74db95
ramesh-stackblitz <rameshkutti23@gmail.com>
2022-07-30 13:06:39 +0530

    2 files modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
-
   title = '';
   constructor() {}
 
