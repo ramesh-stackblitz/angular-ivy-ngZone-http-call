9aeb4a91a6d7d81e9ea868b01bacb465086b56c7
ramesh-stackblitz <rameshkutti23@gmail.com>
2022-07-30 12:48:07 +0530

    1 file modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,20 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ServiceAppService } from './service-app.service';
 import { TrackByComponent } from './track-by/track-by.component';
+import { GrandParentComponent } from './grand-parent/grand-parent.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
-  declarations: [AppComponent, HelloComponent, TrackByComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TrackByComponent,
+    GrandParentComponent,
+    ParentComponent,
+    ChildComponent,
+  ],
   bootstrap: [AppComponent],
   providers: [ServiceAppService],
 })
---
+++ src/app/grand-parent/grand-parent.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grand-parent',
+  templateUrl: './grand-parent.component.html',
+  styleUrls: ['./grand-parent.component.css']
+})
+export class GrandParentComponent implements OnInit {
+  
+  title: 'Some';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
