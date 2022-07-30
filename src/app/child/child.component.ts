2e6f68b6a1fcc668978c3a86aaf6c8e7359c4786
ramesh-stackblitz <rameshkutti23@gmail.com>
2022-07-30 14:54:21 +0530

    1 file modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -23,13 +23,23 @@ export class ChildComponent implements OnInit {
 
   ngOnInit() {
     console.log('child Component loading...');
-    // this.zone.runOutsideAngular(() => {
-    this.counter++;
-    setInterval(() => {
-      let number = Math.random();
-      this.number = number;
-    }, 5000);
-    //});
+    this.zone.runOutsideAngular(() => {
+      this.counter++;
+      setInterval(() => {
+        this.counter++;
+
+        if (this.counter > 10) {
+          this.zone.run(() => {
+            this.number = Math.random();
+          });
+        } else {
+          this.number = Math.random();
+        }
+        // console.log('test....');
+        // let number = Math.random();
+        // this.number = number;
+      }, 1000);
+    });
     // setInterval(() => {
     //this.ref.markForCheck();
     // this.ref.detectChanges();
